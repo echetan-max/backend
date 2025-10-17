e18f23b91f85781c9e945a07acb1cfe6645d55ee
E. Lakshmi Chetan <echetan246@gmail.com>
2025-10-17 22:39:16 +0530

    Delete dummy_server.py

--- dummy_server.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from worker import main as run_worker
-    from threading import Thread
-
-    # Start worker in background
-    Thread(target=run_worker, daemon=True).start()
-
-    # Start Flask to expose a port
-    app.run(host="0.0.0.0", port=10000)
